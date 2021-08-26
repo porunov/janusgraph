e1e22bc058e4854a05e08e46d004d968a7418979
Oleksandr Porunov <alexandr.porunov@gmail.com>
2021-08-26 10:18:47 +0300

    Test DataStax String configuration with all tests
    Signed-off-by: Oleksandr Porunov <alexandr.porunov@gmail.com>

--- janusgraph-cql/src/test/java/org/janusgraph/graphdb/cql/CQLGraphTest.java
+++ janusgraph-cql/src/test/java/org/janusgraph/graphdb/cql/CQLGraphTest.java
@@ -16,6 +16,7 @@ package org.janusgraph.graphdb.cql;
 
 import io.github.artsok.RepeatedIfExceptionsTest;
 import org.janusgraph.JanusGraphCassandraContainer;
+import org.janusgraph.diskstorage.configuration.ConfigElement;
 import org.janusgraph.diskstorage.configuration.WriteConfiguration;
 import org.janusgraph.graphdb.JanusGraphTest;
 import org.junit.jupiter.api.Disabled;
@@ -26,13 +27,20 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.janusgraph.diskstorage.cql.CQLConfigOptions.ATOMIC_BATCH_MUTATE;
+import static org.janusgraph.diskstorage.cql.CQLConfigOptions.BASE_PROGRAMMATIC_CONFIGURATION_ENABLED;
 import static org.janusgraph.diskstorage.cql.CQLConfigOptions.BATCH_STATEMENT_SIZE;
 import static org.janusgraph.diskstorage.cql.CQLConfigOptions.EXECUTOR_SERVICE_ENABLED;
+import static org.janusgraph.diskstorage.cql.CQLConfigOptions.LOCAL_DATACENTER;
+import static org.janusgraph.diskstorage.cql.CQLConfigOptions.STRING_CONFIGURATION;
 import static org.janusgraph.graphdb.configuration.GraphDatabaseConfiguration.ASSIGN_TIMESTAMP;
+import static org.janusgraph.graphdb.configuration.GraphDatabaseConfiguration.CONNECTION_TIMEOUT;
+import static org.janusgraph.graphdb.configuration.GraphDatabaseConfiguration.STORAGE_HOSTS;
+import static org.janusgraph.graphdb.configuration.GraphDatabaseConfiguration.STORAGE_PORT;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
@@ -41,9 +49,48 @@ public class CQLGraphTest extends JanusGraphTest {
     @Container
     public static final JanusGraphCassandraContainer cqlContainer = new JanusGraphCassandraContainer();
 
+    private static final String DATASTAX_JAVA_DRIVER_STRING_CONFIGURATION_PATTERN =
+        "datastax-java-driver {\n" +
+            "advanced.address-translator.class = \"PassThroughAddressTranslator\"\n" +
+            "basic.contact-points = [ \"${hostname}:${port}\" ]\n" +
+            "basic.session-name = JanusGraphCQLSession\n" +
+            "basic.load-balancing-policy{\n" +
+            "    local-datacenter = \"${datacenter}\"\n" +
+            "}\n" +
+            "basic.request.timeout = ${timeout} milliseconds\n"+
+            "advanced.connection.connect-timeout = ${timeout} milliseconds\n"+
+            "advanced.connection.pool.local.size = 1\n"+
+            "advanced.connection.pool.remote.size = 1\n"+
+            "advanced.connection.max-requests-per-connection = 1024\n"+
+            "advanced.metadata.schema.debouncer.window = 0\n"+
+            "advanced.netty.io-group.size = 0\n"+
+            "advanced.netty.admin-group.size = 0\n"+
+            "advanced.netty.io-group.shutdown.quiet-period = 0\n"+
+            "advanced.netty.admin-group.shutdown.quiet-period = 0\n"+
+            "}\n";
+
     @Override
     public WriteConfiguration getConfiguration() {
-        return cqlContainer.getConfiguration(getClass().getSimpleName()).getConfiguration();
+
+        WriteConfiguration wc = cqlContainer.getConfiguration(getClass().getSimpleName()).getConfiguration();
+
+        String[] hostname = wc.get(ConfigElement.getPath(STORAGE_HOSTS), String[].class);
+        String port = wc.get(ConfigElement.getPath(STORAGE_PORT), Integer.class).toString();
+        Duration timeout = wc.get(ConfigElement.getPath(CONNECTION_TIMEOUT), Duration.class);
+        if(timeout == null){
+            timeout = CONNECTION_TIMEOUT.getDefaultValue();
+        }
+
+        String preparedDataStaxConfiguration = DATASTAX_JAVA_DRIVER_STRING_CONFIGURATION_PATTERN
+            .replace("${hostname}", hostname[0])
+            .replace("${port}", port)
+            .replace("${datacenter}", LOCAL_DATACENTER.getDefaultValue())
+            .replace("${timeout}", String.valueOf(timeout.toMillis()));
+
+        wc.set(ConfigElement.getPath(BASE_PROGRAMMATIC_CONFIGURATION_ENABLED), false);
+        wc.set(ConfigElement.getPath(STRING_CONFIGURATION), preparedDataStaxConfiguration);
+
+        return wc;
     }
 
     @Test
